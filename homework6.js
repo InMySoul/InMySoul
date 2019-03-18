a6482c707effdf0360d91f3a7c85a81fff18f54c
InMySoul <47898587+InMySoul@users.noreply.github.com>
2019-03-18 16:18:56 +0200

    Add files via upload

---
+++ homework6.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var table = document.body.children[0];
+for (var i = 0; i < table.rows.length; i++)
+{
+    var row = table.rows[i];
+    row.cells[i].style.backgroundColor = '#dd0c12';
+}
