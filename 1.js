5984bfafa2b141804c54e40ae1f1f7893ff89297
InMySoul <47898587+InMySoul@users.noreply.github.com>
2019-03-18 16:17:02 +0200

    Delete 1.js

--- 1.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-//У нас есть массив goods. Сколько в нем элементов – не знаем, но можем прочитать из goods.length.
-//Напишите код для получения последнего элемента goods.
-    var goods = [1,2,3];
-    var lastItem = goods[goods.length - 1];
-    alert (lastItem);
-    goods[goods.length] + 'Компьютер';
