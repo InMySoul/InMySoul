7f7a5c8a8f69530e1c5a7a6e728fe0b99d22f687
InMySoul <47898587+InMySoul@users.noreply.github.com>
2019-03-22 17:30:40 +0200

    Delete homework6.js

--- homework6.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var table = document.body.children[0];
-for (var i = 0; i < table.rows.length; i++)
-{
-    var row = table.rows[i];
-    row.cells[i].style.backgroundColor = '#dd0c12';
-}
